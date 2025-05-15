1fff303f6c18131df549081511662490b43df49a
이순용 <ssun@isun-yong-ui-MacBookPro.local>
2025-05-15 10:26:27 +0900

    initial commit for sgh-chatbot

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -45,7 +45,7 @@ export default function Chat() {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:4000/chat', {
+      const res = await axios.post('https://sgh-chatbot.onrender.com/chat', {
         message: messageToSend
       });
       const reply = res.data.reply;
